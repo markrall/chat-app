0ff480655f86688bc1a7399ba4056c7019021efb
Mark <62776921+productbymark@users.noreply.github.com>
2020-07-20 06:17:01 +1000

    Init commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+const publicDIR = path.join(__dirname, '../public')
+
+app.use(express.static(publicDIR))
+
+app.get('/', (req, res) => {
+    res.render('index', () => {
+
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Chat App up on port ${port}`)
+})
